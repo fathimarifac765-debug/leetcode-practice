2e6ddd14970a9321689109250558d18a0e0ebccd
Fathima Rifa <fathimarifac765@gmail.com>
2026-09-25 10:04:57 +0530

    Time: 10 ms (12.5%), Space: 58.9 MB (6.94%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let map = {};
+
+    for (let num of nums) {
+        map[num] = (map[num] || 0) + 1;
+    }
+
+    for (let key in map) {
+        if (map[key] === 1) {
+            return Number(key);
+        }
+    }
+};
